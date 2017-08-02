10202e6100fd08bc4b7f885150579764c23710d3
Hoon Ahn <shahn801@gmail.com>
2017-08-02 12:05:19 +0900

    Remove exGame

---
+++ Penguin Escape/Penguin Escape/Ground.swift
@@ -0,0 +1,9 @@
+//
+//  Ground.swift
+//  Penguin Escape
+//
+//  Created by Sanghoon Ahn on 2017. 7. 27..
+//  Copyright © 2017년 AHN. All rights reserved.
+//
+
+import Foundation
---
+++ Penguin Escape/Penguin Escape/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  Penguin Escape
+//
+//  Created by Sanghoon Ahn on 2017. 7. 30..
+//  Copyright © 2017년 AHN. All rights reserved.
+//
+
+import Foundation
